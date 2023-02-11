37142734fb0872c0758ea59e12282c6abe3fbaa6
Bhangure <bhangureasha853@gmail.com>
2023-02-11 11:01:00 +0530

    this files i want to commit

---
+++ src/test/java/com/example/Springboot1st/Springboot1stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot1st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
